44a7e21e5e2340866e3ce7ed6f9c6b5d9cd9a3fe
Dan <d9a9n9i9e9l9@hotmail.com>
2019-10-12 13:57:14 -0400

    Some UI stuff

--- app/src/main/java/com/example/stressos/MainStuff/ActuallyDoingStuff/HealthActivity.kt
+++ app/src/main/java/com/example/stressos/MainStuff/ActuallyDoingStuff/HealthActivity.kt
@@ -5,24 +5,29 @@ import android.view.LayoutInflater
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.stressos.R
 
 import kotlinx.android.synthetic.main.activity_health.*
+import kotlinx.android.synthetic.main.content_health.*
+import kotlinx.android.synthetic.main.content_health.view.*
 
 class HealthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_health)
+
+        val text = "45"     //Hardcoded for now, change with function name
+
+        TextViewHRUpdate.text = text
     }
 
-    override fun onEnterAnimationComplete() {
-        super.onEnterAnimationComplete()
-    }
+    /*fun HeartBeat(val text: String){
+        fun bind(viewHolder: RecyclerView.ViewHolder){
+            viewHolder.itemView.TextViewHRUpdate.text = text
+
+            val updated = viewHolder.itemView.TextViewHRUpdate
+        }
+    }*/
 }
-
-/*class ScreenSlide : Fragment(){
-    override fun onCreateView(
-        inflater: LayoutInflater
-    )
-}*/
